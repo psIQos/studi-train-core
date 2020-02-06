a9a9dcf0b752c29adcf8c83d6db86d0725d2eea1
Julian Mathis <julianmathis95@gmail.com>
2020-02-06 11:27:25 +0100

    test: use provided port

--- StudiTrain/Program.cs
+++ StudiTrain/Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace StudiTrain
 {
@@ -17,8 +11,12 @@ namespace StudiTrain
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://*:" + port);
+        }
     }
 }
