ee2c4942661d334eca572141a57a01987546d09d
Julian Mathis <julianmathis95@gmail.com>
2020-02-12 12:32:47 +0100

    set conn string from heroku correctly

---
+++ StudiTrain/Controllers/QuestionController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudiTrain.Models;
+using StudiTrain.Setup;
+
+namespace StudiTrain.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly PostgresContext _db;
+
+        public QuestionController(ControllerSetup setup)
+        {
+            if (setup.ConnectionString == null)
+                throw new InvalidOperationException("Connection string has to be set by environment variables.");
+            var optionsBuilder = new DbContextOptionsBuilder<PostgresContext>();
+            optionsBuilder.UseNpgsql(setup.ConnectionString);
+            _db = new PostgresContext(optionsBuilder.Options);
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<DbSet<Questions>> Get()
+        {
+            return _db.Questions;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Questions> Get(int id)
+        {
+            return _db.Questions.First(q => q.Id == id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Questions question)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ StudiTrain/Setup/ControllerSetup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Configuration;
+
+namespace StudiTrain.Setup
+{
+    public class ControllerSetup
+    {
+        public string ConnectionString { get; set; }
+
+        public ControllerSetup(IConfiguration conf)
+        {
+            ConnectionString = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development"
+                ? conf["ConnectionString"]
+                : PostgresToConnectionString(Environment.GetEnvironmentVariable("DATABASE_URL"));
+        }
+
+        /// <summary>
+        /// transform a connection string from libpq form to standard syntax
+        /// </summary>
+        /// <param name="libpqString"></param>
+        /// <exception cref="ArgumentException">if the passed string is not of format "user:password@netloc:port/dbname"</exception>
+        /// <returns></returns>
+        private static string PostgresToConnectionString(string libpqString)
+        {
+            var leadIndex = libpqString.IndexOf("://", StringComparison.Ordinal);
+            var paramDeclarations = new []{"Username=", "Password", "Host", "Port", "Database"};
+            var paramValues = libpqString.Substring(leadIndex + 3).Split(new[]{ ':', '@'});
+
+            if (paramValues.Length != 5) throw new ArgumentException("libpq string is of the wrong format");
+
+            return string.Join(";", paramDeclarations.Zip(paramValues, (decl, val) => decl + "=" + val))
+                 + ";SslMode=Require;Trust Server Certificate=True";
+
+        }
+    }
+}
